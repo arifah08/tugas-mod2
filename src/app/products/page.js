7d2d06f5354d43714b9468bcafccc8c2cd75c1f4
Arifah Putri Solihah <arifahputrisholihah.arifah08@gmail.com>
2023-12-10 12:50:51 +0700

    fist

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,23 +1,22 @@
 // *use client*
 
-import { createClient } from "contentful";
+// import { createClient } from "contentful";
 
-export async function getData() {
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  });
+// export async function getData() {
+//   const client = createClient({
+//     space: process.env.CONTENTFUL_SPACE_ID,
+//     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+//   });
 
-  const res = await client.getEntries({ content_type: "products" });
+//   const res = await client.getEntries({ content_type: "products" });
 
-  return {
-    props: {
-      products: res.items,
-    },
-  };
-}
+//   return {
+//     props: {
+//       products: res.items,
+//     },
+//   };
+// }
 
-export default function Products(products) {
-  console.log(products);
+export default function Products() {
   return <div>Products List</div>;
 }
